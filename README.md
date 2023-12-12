3bd98c5f431f3c3075787cbb09241efafcd9d57f
Choigunwoo <gunwoo8873@outlook.kr>
2023-12-12 10:27:19 +0900

    -

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Sprint-Project
+
+## Discord.js (V 0.0.1)
+
+1. Sprint Project 제작 기간 2023 / 12 / 10 ~  
+    App.js => Main Discord Bot OnReady
+2. Fix Note  
+    * ClientId => BotId
+    * GuildId => ServerId
---
+++ Web/index.js
@@ -0,0 +1,9 @@
+// Head-MenuButton Class
+class ClassName {
+    constructor(Div_ID, Text_Content, Background_Color) {
+        // Div ID
+        this.Container_ID = document.getElementById(Div_ID);
+
+        this.
+    }
+}
