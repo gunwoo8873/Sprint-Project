89e1cec71df778ebd3948c8655ebfce2472bd945
Perez <gunwoo8873@outlook.kr>
2023-12-23 12:58:27 +0900

    Remove File

--- Web/index.js
+++
@@ -1,9 +0,0 @@
-// Head-MenuButton Class
-class ClassName {
-    constructor(Div_ID, Text_Content, Background_Color) {
-        // Div ID
-        this.Container_ID = document.getElementById(Div_ID);
-
-        this.
-    }
-}
